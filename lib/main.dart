7865f7a95ee35aff8fe5be9509e1ea630c93888a
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-01-08 15:03:36 +0300

    Add ListView & Card to view the Recipe image/text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'recipe.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -41,8 +42,24 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Container(),
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index){
+            return buildRecipeCard(Recipe.samples[index]);
+          }),
       )
     );
   }
+
+  Widget buildRecipeCard (Recipe recipe){
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          Text(recipe.label)
+        ],
+      )
+    );
+  }
+
 }
